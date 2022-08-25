5384f058eda9d007457153aa3fe619cc3b2f0faf
calvinsims <thecalvins.simbolon@gmail.com>
2022-08-25 12:48:46 +0700

    Frontend tahap 1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["public/**/*.{html,js,php}"],
+  theme: {
+    letterSpacing: {
+      custom: '0.3%',
+    }
+
+    // extend: {},
+  },
+  plugins: [],
+}
